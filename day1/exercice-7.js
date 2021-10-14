3835a3c6f9fa2f4961d4892318e3b78bc572c0ff
moko-se <sergenzuzi@outlook.fr>
2021-10-14 09:31:17 +0200

    Exercice day1

--- day1/exercice-7.js
+++ day1/exercice-7.js
@@ -3,9 +3,9 @@ export const my_is_posi_neg = (nbr) => {
 
     if (nbr < 0) {
         return 'NEGATIF';
-    } else if (nbr == 0) {
+    } else if (nbr == 0 ) {
         return 'NEUTRAL';
-    } else {
+    } else if (nbr > 0 || nbr === undefined || nbr === null) {
         return 'POSITIF';
     }
 }
